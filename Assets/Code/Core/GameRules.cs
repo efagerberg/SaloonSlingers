95e62808e149cb2c123e2b6eb7710f9988c5d0e3
Evan Fagerberg <adioevan@gmail.com>
2023-04-30 20:51:05 -0400

    Add back newtonsoft references in core assembly

--- Assets/Code/Core/GameRules.cs
+++ Assets/Code/Core/GameRules.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-using SaloonSlingers.Core.HandEvaluators;
+using Newtonsoft.Json;
 
-using Unity.Plastic.Newtonsoft.Json;
+using SaloonSlingers.Core.HandEvaluators;
 
 
 namespace SaloonSlingers.Core
