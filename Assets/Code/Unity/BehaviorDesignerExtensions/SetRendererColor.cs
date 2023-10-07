d919d77f6050b89b4856549ae7f20e7625ada823
Evan Fagerberg <adioevan@gmail.com>
2023-10-07 02:54:56 -0400

    Convert to use behavior tree for detecting player and pursuing as well as patrol

---
+++ Assets/Code/Unity/BehaviorDesignerExtensions/SetRendererColor.cs
@@ -0,0 +1,10 @@
+//using BehaviorDesigner.Runtime;
+//using BehaviorDesigner.Runtime.Tasks;
+
+//namespace SaloonSlingers.Unity
+//{
+//    public class SetRendererColor : Action
+//    {
+//        public SharedColor color;
+//    }
+//}
