85699490a4cf4d2dc590c5b65adecd7655129cc5
Evan Fagerberg <adioevan@gmail.com>
2023-10-09 13:13:45 -0400

    Make scene transition consistent
    LoadSceneAsync causes a delay so fade out first
    
    Also fix enemy behavior to pursue, rotate, and draw at the same time

--- Assets/Code/Unity/Utils/SceneLoader.cs
+++ Assets/Code/Unity/Utils/SceneLoader.cs
@@ -18,38 +18,40 @@ namespace SaloonSlingers.Unity
 
         public void LoadScene(string sceneName)
         {
+            if (SceneManager.GetActiveScene().name == sceneName || loadOperation != null) return;
             StartCoroutine(PerformSceneTransition(sceneName));
         }
 
         public IEnumerator PerformSceneTransition(string sceneName)
         {
-            if (SceneManager.GetActiveScene().name != sceneName && loadOperation == null)
+            if (TransitionImage != null)
             {
-                loadOperation = SceneManager.LoadSceneAsync(sceneName);
-                loadOperation.allowSceneActivation = false;
-                if (TransitionImage != null)
-                {
-                    TransitionImage.gameObject.SetActive(true);
-                    yield return Fader.FadeTo(TransitionImage, 1, transitionDuration / 2f);
-                }
-                loadOperation.allowSceneActivation = true;
-                while (!loadOperation.isDone)
-                    yield return null;
-
-                if (TransitionImage != null)
-                {
-                    Color c = TransitionImage.color;
-                    c.a = 1;
-                    TransitionImage.color = c;
-                    TransitionImage.gameObject.SetActive(true);
-                    yield return Fader.FadeTo(TransitionImage, 0, transitionDuration / 2f);
-                    TransitionImage.gameObject.SetActive(false);
-                    var originalColor = TransitionImage.color;
-                    originalColor.a = 0;
-                    TransitionImage.color = originalColor;
-                }
-                loadOperation = null;
+                TransitionImage.gameObject.SetActive(true);
+                yield return Fader.FadeTo(TransitionImage, 1, transitionDuration / 2f);
             }
+            loadOperation = SceneManager.LoadSceneAsync(sceneName);
+            loadOperation.allowSceneActivation = false;
+
+            while (!loadOperation.isDone)
+            {
+                if (loadOperation.progress >= 0.9f)
+                    loadOperation.allowSceneActivation = true;
+                yield return null;
+            }
+
+            if (TransitionImage != null)
+            {
+                Color c = TransitionImage.color;
+                c.a = 1;
+                TransitionImage.color = c;
+                TransitionImage.gameObject.SetActive(true);
+                yield return Fader.FadeTo(TransitionImage, 0, transitionDuration / 2f);
+                TransitionImage.gameObject.SetActive(false);
+                var originalColor = TransitionImage.color;
+                originalColor.a = 0;
+                TransitionImage.color = originalColor;
+            }
+            loadOperation = null;
         }
     }
 }
