34d7317ca4216c10988520796a59af0e9e1cb196
Evan Fagerberg <adioevan@gmail.com>
2024-01-03 23:19:37 -0500

    SLING-73 | Money gain and loss UI event handling

--- Assets/Code/Unity/Actor/Player/PlayerAttributeUI.cs
+++ Assets/Code/Unity/Actor/Player/PlayerAttributeUI.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 using SaloonSlingers.Core;
 
 using TMPro;
@@ -15,55 +17,116 @@ namespace SaloonSlingers.Unity
         private TextMeshProUGUI healthPercentText;
         [SerializeField]
         private TextMeshProUGUI moneyText;
+        [SerializeField]
+        private AudioClip moneyLostSFX;
+        [SerializeField]
+        private AudioClip moneyGainedSFX;
+        [SerializeField]
+        private AudioSource audioSource;
+        [SerializeField]
+        private float moneyDeltaPresentationTime;
 
         private Points hitPoints;
         private Points money;
+        private WaitForSeconds deltaDelay;
+        private bool x = false;
+
+        private UIData uiData;
 
         private void Awake()
         {
             var attributes = LevelManager.Instance.Player.GetComponent<Attributes>();
             hitPoints = attributes.Registry[AttributeType.Health];
-            hitPoints.Increased += UpdateHealthBar;
-            hitPoints.Decreased += UpdateHealthBar;
-            UpdateFill(healthBar, hitPoints);
-            healthPercentText.text = hitPoints.AsPercent().ToString("P0");
-
             money = attributes.Registry[AttributeType.Money];
-            money.Increased += UpdateMoneyText;
-            money.Decreased += UpdateMoneyText;
+            deltaDelay = new(moneyDeltaPresentationTime);
+
+            uiData = new()
+            {
+                HealthBar = healthBar,
+                HealthPercentText = healthPercentText,
+                AudioSource = audioSource,
+                MoneyLostSFX = moneyLostSFX,
+                MoneyGainedSFX = moneyGainedSFX,
+                MoneyText = moneyText
+            };
+            PlayerAttributeUICoordinator.UpdateHealthBar(uiData, hitPoints);
             moneyText.text = money.Value.ToString();
+
+            hitPoints.Increased += OnHealthChanged;
+            hitPoints.Decreased += OnHealthChanged;
+            money.Increased += OnMoneyChanged;
+            money.Decreased += OnMoneyChanged;
+
+            InvokeRepeating(nameof(f), 0, 2f);
+        }
+
+        private void f()
+        {
+            if (x) money.Increase(10);
+            else money.Decrease(10);
+            x = !x;
         }
 
         private void OnEnable()
         {
-            hitPoints.Increased += UpdateHealthBar;
-            hitPoints.Decreased += UpdateHealthBar;
-            money.Increased += UpdateMoneyText;
-            money.Decreased += UpdateMoneyText;
+            hitPoints.Increased += OnHealthChanged;
+            hitPoints.Decreased += OnHealthChanged;
+            money.Increased += OnMoneyChanged;
+            money.Decreased += OnMoneyChanged;
         }
 
         private void OnDisable()
         {
-            hitPoints.Increased -= UpdateHealthBar;
-            hitPoints.Decreased -= UpdateHealthBar;
-            money.Increased -= UpdateMoneyText;
-            money.Decreased -= UpdateMoneyText;
+            hitPoints.Increased -= OnHealthChanged;
+            hitPoints.Decreased -= OnHealthChanged;
+            money.Increased -= OnMoneyChanged;
+            money.Decreased -= OnMoneyChanged;
         }
 
-        private void UpdateMoneyText(IReadOnlyPoints sender, ValueChangeEvent<uint> e)
+        private void OnMoneyChanged(IReadOnlyPoints sender, ValueChangeEvent<uint> e)
         {
-            moneyText.text = e.After.ToString();
+            var coroutine = PlayerAttributeUICoordinator.MoneyChangeEffect(uiData, e, deltaDelay);
+            StartCoroutine(coroutine);
         }
 
-        private void UpdateHealthBar(IReadOnlyPoints sender, ValueChangeEvent<uint> e)
+        private void OnHealthChanged(IReadOnlyPoints sender, ValueChangeEvent<uint> e)
         {
-            UpdateFill(healthBar, sender);
-            healthPercentText.text = sender.AsPercent().ToString("P0");
+            PlayerAttributeUICoordinator.UpdateHealthBar(uiData, sender);
+        }
+    }
+
+    public static class PlayerAttributeUICoordinator
+    {
+        public static IEnumerator MoneyChangeEffect(UIData data,
+                                                    ValueChangeEvent<uint> moneyChangedEvent,
+                                                    WaitForSeconds deltaDelay)
+        {
+            bool decreased = moneyChangedEvent.Before > moneyChangedEvent.After;
+            uint delta = decreased ? moneyChangedEvent.Before - moneyChangedEvent.After : moneyChangedEvent.After - moneyChangedEvent.Before;
+            char deltaChar = decreased ? '-' : '+';
+            if (decreased) data.AudioSource.PlayOneShot(data.MoneyLostSFX);
+            else data.AudioSource.PlayOneShot(data.MoneyGainedSFX);
+
+            data.MoneyText.text = $"{deltaChar}{delta}";
+            yield return deltaDelay;
+
+            data.MoneyText.text = moneyChangedEvent.After.ToString();
         }
 
-        private static void UpdateFill(Image image, IReadOnlyPoints points)
+        public static void UpdateHealthBar(UIData data, IReadOnlyPoints hitPoints)
         {
-            image.fillAmount = points.AsPercent();
+            data.HealthBar.fillAmount = hitPoints.AsPercent();
+            data.HealthPercentText.text = hitPoints.AsPercent().ToString("P0");
         }
+    }
+
+    public struct UIData
+    {
+        public Image HealthBar;
+        public TextMeshProUGUI HealthPercentText;
+        public AudioSource AudioSource;
+        public AudioClip MoneyLostSFX;
+        public AudioClip MoneyGainedSFX;
+        public TextMeshProUGUI MoneyText;
     }
 }
