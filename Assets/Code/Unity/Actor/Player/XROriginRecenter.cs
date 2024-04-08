2d3e96a46fef82b58aebef4c937223ffef56cedc
Evan Fagerberg <adioevan@gmail.com>
2024-04-07 22:02:36 -0400

    Fix recenter script

--- Assets/Code/Unity/Actor/Player/XROriginRecenter.cs
+++ Assets/Code/Unity/Actor/Player/XROriginRecenter.cs
@@ -9,16 +9,27 @@ namespace SaloonSlingers.Unity.Actor
         public Transform OrientationReference;
         public XROrigin Origin;
 
+        private bool initialRecenteringPerformed = false;
+
         public void Recenter()
         {
             Origin.MatchOriginUpCameraForward(OrientationReference.up, OrientationReference.forward);
             Origin.MoveCameraToWorldLocation(OrientationReference.position + new Vector3(0, Origin.CameraInOriginSpaceHeight, 0));
         }
 
-        private void Start()
+        private void Awake()
         {
             if (Origin == null) Origin = GetComponent<XROrigin>();
-            Recenter();
+        }
+
+        private void LateUpdate()
+        {
+            // Just doing this on Awake or Start is too late to take effect
+            if (!initialRecenteringPerformed)
+            {
+                Recenter();
+                initialRecenteringPerformed = true;
+            }
         }
     }
 }
--- Assets/Code/Unity/Tests/Actor/XROriginRecentererTests.cs
+++ Assets/Code/Unity/Tests/Actor/XROriginRecentererTests.cs
@@ -13,41 +13,34 @@ using UnityEngine.TestTools.Utils;
 public class XROriginRecentererTests
 {
     [Test]
-    public void CentersXROrigin_OnReference()
+    public void CentersXROrigin_WhenRecenterCall()
     {
-        var subjectGO = new GameObject();
-        var camera = subjectGO.AddComponent<Camera>();
-        var origin = subjectGO.AddComponent<XROrigin>();
-        origin.Camera = camera;
-        var referenceGO = new GameObject();
-        referenceGO.transform.SetPositionAndRotation(
-            new Vector3(100, 2, -20),
-            Quaternion.Euler(123, 321, 123)
-        );
-        subjectGO.transform.SetPositionAndRotation(
-            new Vector3(1, 2, 3),
-            Quaternion.Euler(90, 1, 123)
-        );
-        var subject = subjectGO.AddComponent<XROriginRecenter>();
+        XROriginRecenter subject = BuildSubject(out Camera camera, out XROrigin origin, out GameObject referenceGO);
         subject.OrientationReference = referenceGO.transform;
         subject.Origin = origin;
         subject.Recenter();
 
-        var comparer = new Vector3EqualityComparer(10e-6f);
-        Assert.That(origin.transform.up, Is.EqualTo(referenceGO.transform.up).Using(comparer));
-        Assert.That(camera.transform.forward, Is.EqualTo(referenceGO.transform.forward).Using(comparer));
-        Assert.That(camera.transform.position.y, Is.EqualTo(referenceGO.transform.position.y + origin.CameraInOriginSpaceHeight));
+        AssertCentered(camera, origin, referenceGO);
     }
 
-    [RequiresPlayMode]
     [UnityTest]
-    public IEnumerator Start_CentersXROrigin_OnReference()
+    public IEnumerator CentersXROrigin_AsPartOfInitialization()
+    {
+        XROriginRecenter subject = BuildSubject(out Camera camera, out XROrigin origin, out GameObject referenceGO);
+        subject.runInEditMode = true;
+        subject.OrientationReference = referenceGO.transform;
+        yield return null;
+
+        AssertCentered(camera, origin, referenceGO);
+    }
+
+    private static XROriginRecenter BuildSubject(out Camera camera, out XROrigin origin, out GameObject referenceGO)
     {
         var subjectGO = new GameObject();
-        var camera = subjectGO.AddComponent<Camera>();
-        var origin = subjectGO.AddComponent<XROrigin>();
+        camera = subjectGO.AddComponent<Camera>();
+        origin = subjectGO.AddComponent<XROrigin>();
         origin.Camera = camera;
-        var referenceGO = new GameObject();
+        referenceGO = new GameObject();
         referenceGO.transform.SetPositionAndRotation(
             new Vector3(100, 2, -20),
             Quaternion.Euler(123, 321, 123)
@@ -56,10 +49,11 @@ public class XROriginRecentererTests
             new Vector3(1, 2, 3),
             Quaternion.Euler(90, 1, 123)
         );
-        var subject = subjectGO.AddComponent<XROriginRecenter>();
-        subject.OrientationReference = referenceGO.transform;
-        yield return null;
+        return subjectGO.AddComponent<XROriginRecenter>();
+    }
 
+    private static void AssertCentered(Camera camera, XROrigin origin, GameObject referenceGO)
+    {
         var comparer = new Vector3EqualityComparer(10e-6f);
         Assert.That(origin.transform.up, Is.EqualTo(referenceGO.transform.up).Using(comparer));
         Assert.That(camera.transform.forward, Is.EqualTo(referenceGO.transform.forward).Using(comparer));
